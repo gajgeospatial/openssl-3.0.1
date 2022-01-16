d33bdc671ee5a1ca42da20ea87c033c1b9f7ff28
gajgeospatial <glen.johnson@gaj-geospatial.com>
2022-01-15 19:31:21 -0500

    Add ignore files and updated move scripts
    Add ignore files and updated move scripts

--- MoveDebug.bat
+++ MoveDebug.bat
@@ -7,7 +7,7 @@ if exist libcrypto-3-x64.dll move /Y libcrypto-3-x64.dll vc143\x64\vs2022_Debug\
 if exist libcrypto-3-x64.ilk move /Y libcrypto-3-x64.ilk vc143\x64\vs2022_Debug\libcrypto-3-x64.ilk
 if exist libcrypto-3-x64.pdb move /Y libcrypto-3-x64.pdb vc143\x64\vs2022_Debug\libcrypto-3-x64.pdb
 if exist libssl.lib move /Y libssl.lib vc143\x64\vs2022_Debug\libssl.lib
-if exist libssl_staic.lib move /Y libssl_static.lib vc143\x64\vs2022_Debug\libssl_static.lib
+if exist libssl_static.lib move /Y libssl_static.lib vc143\x64\vs2022_Debug\libssl_static.lib
 if exist libssl-3-x64.dll move /Y libssl-3-x64.dll vc143\x64\vs2022_Debug\libssl-3-x64.dll
 if exist libssl-3-x64.ilk move /Y libssl-3-x64.ilk vc143\x64\vs2022_Debug\libssl-3-x64.ilk
 if exist libssl-3-x64.pdb move /Y libssl-3-x64.pdb vc143\x64\vs2022_Debug\libssl-3-x64.pdb
--- MoveRelease.bat
+++ MoveRelease.bat
@@ -7,7 +7,7 @@ if exist libcrypto-3-x64.dll move /Y libcrypto-3-x64.dll vc143\x64\vs2022_Releas
 if exist libcrypto-3-x64.ilk move /Y libcrypto-3-x64.ilk vc143\x64\vs2022_Release\libcrypto-3-x64.ilk
 if exist libcrypto-3-x64.pdb move /Y libcrypto-3-x64.pdb vc143\x64\vs2022_Release\libcrypto-3-x64.pdb
 if exist libssl.lib move /Y libssl.lib vc143\x64\vs2022_Release\libssl.lib
-if exist libssl_staic.lib move /Y libssl_static.lib vc143\x64\vs2022_Release\libssl_static.lib
+if exist libssl_static.lib move /Y libssl_static.lib vc143\x64\vs2022_Release\libssl_static.lib
 if exist libssl-3-x64.dll move /Y libssl-3-x64.dll vc143\x64\vs2022_Release\libssl-3-x64.dll
 if exist libssl-3-x64.ilk move /Y libssl-3-x64.ilk vc143\x64\vs2022_Release\libssl-3-x64.ilk
 if exist libssl-3-x64.pdb move /Y libssl-3-x64.pdb vc143\x64\vs2022_Release\libssl-3-x64.pdb
